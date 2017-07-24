6cf0fa30ed0f2cd92a1ecb4db6669ad593599cbf
listicos <listico__@hotmail.com>
2017-07-24 14:12:08 -0500

    Keyboard type

--- index.ios.js
+++ index.ios.js
@@ -1,12 +1,17 @@
-import {
-    AlertIOS
-} from 'react-native';
+import { AlertIOS } from 'react-native'
 
 export default function prompt(
-    title: ?string,
-    message?: ?string,
-    callbackOrButtons?: ?((text: string) => void) | Object,
-    options?: Object
+  title: ?string,
+  message?: ?string,
+  callbackOrButtons?: ?(text: string) => void | Object,
+  options?: Object
 ): void {
-    AlertIOS.prompt(title, message, callbackOrButtons, options.type, options.defaultValue);
-};
+  AlertIOS.prompt(
+    title,
+    message,
+    callbackOrButtons,
+    options.type,
+    options.defaultValue,
+    options.keyboardType
+  )
+}
